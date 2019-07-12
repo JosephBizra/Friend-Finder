7ade5d8ab8782606ad1fe7595fc246206cb62479
JosephBizra <j.bizra@gmail.com>
2019-07-12 14:07:22 -0500

    edited port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var path = require("path");
 
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 app.use(express.urlencoded({extended: true}));
